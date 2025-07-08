4957691992245e28fa4d778ed7d97f10ad869b73
pearlgw <gayuhterluv@gmail.com>
2025-07-08 09:23:24 +0700

    update token in route api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -31,7 +31,7 @@ export const authOptions: NextAuthOptions = {
               name: user.fullname,
               email: user.email,
               role: role,
-              token: user.access_token,
+              token: token,
             };
           }
 
